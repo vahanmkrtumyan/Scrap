599a32917a35ef1016986f4a14d755a12ac935df
vahanmkrtumyan <35697975+vahanmkrtumyan@users.noreply.github.com>
2019-01-27 23:24:33 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,37 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 const Json2csvParser = require("json2csv").Parser;
 const puppeteer = require("puppeteer");
+var json2xls = require('json2xls');
 
 const URL = "https://internetbank.armbusinessbank.am/InternetBank/MainForm.wgx";
 
 (async () => {
+
+let hashivner = [];
+
   const browser = await puppeteer.launch({
     args: ["--disable-features=site-per-process"],
     headless: false
   });
+
   
+
+  let companies = [
+    { username: "miparti1", password: "123456" },
+    { username: "telectro1", password: "3215987"},
+    { username: "tfurniture2", password: "3215987"},
+    { username: "tindustry2", password: "3215987"}
+  ];
+
   const page = await browser.newPage();
+for (c of companies) {
+  
   await page.goto(
     "https://internetbank.armbusinessbank.am/internetbank/MainForm.wgx"
   );
   await page.waitForSelector("#TRG_32");
-  await page.type("#TRG_32", "miparti1");
-  await page.type("#TRG_38", "123456");
+  await page.type("#TRG_32", c.username);
+  await page.type("#TRG_38", c.password);
   await page.keyboard.press("Enter");
 
   await page.waitForSelector("#VWGLE_100");
@@ -72,7 +87,12 @@ const URL = "https://internetbank.armbusinessbank.am/InternetBank/MainForm.wgx";
       `table[class= "cb58 cb54 cct32"] > tbody > tr:nth-child(4) > td:nth-child(10) > div >span`
     ).innerText;
   });
-  
+
+  let Անվանում = await page.evaluate(() => {
+    return document.querySelector(
+      `table[class= "cb58 cb54 cct32"] > tbody > tr:nth-child(1) > td:nth-child(6) > div >span`
+    ).innerText;
+  });
 
   let AMD = 0,
     USD = 0,
@@ -149,49 +169,58 @@ const URL = "https://internetbank.armbusinessbank.am/InternetBank/MainForm.wgx";
     // code block
   }
 
-
-  
-    // let cur5 = await page.evaluate(() => {
-    //   return (
-    //     document.querySelector(
-    //       `table[class= "cb58 cb54 cct32"] > tbody > tr:nth-child(5) > td:nth-child(4) > div >span`
-    //     ).innerText || 0
-    //   );
-    // });
-    // let cur5val = await page.evaluate(() => {
-    //   return document.querySelector(
-    //     `table[class= "cb58 cb54 cct32"] > tbody > tr:nth-child(5) > td:nth-child(10) > div >span`
-    //   ).innerText || 0;
-    // });
-
-
-
-    // switch(cur5) {
-    //   case "AMD":
-    //     AMD = AMD + parseFloat(cur5val.replace(/,/g, ''));
-    //     break;
-    //     case "USD":
-    //     USD = USD + parseFloat(cur5val.replace(/,/g, ''));
-    //     break;
-    //     case "RUB":
-    //     RUB = RUB + parseFloat(cur5val.replace(/,/g, ''));
-    //     break;
-    //     case "EUR":
-    //     EUR = EUR + parseFloat(cur5val.replace(/,/g, ''));
-    //     break;
-
-    //   default:
-    //     // code block
-    // }
   
 
+  if (c.username === "telectro1") {
+    let cur5 = await page.evaluate(() => {
+      return (
+        document.querySelector(
+          `table[class= "cb58 cb54 cct32"] > tbody > tr:nth-child(5) > td:nth-child(4) > div >span`
+        ).innerText || 0
+      );
+    });
+    let cur5val = await page.evaluate(() => {
+      return (
+        document.querySelector(
+          `table[class= "cb58 cb54 cct32"] > tbody > tr:nth-child(5) > td:nth-child(10) > div >span`
+        ).innerText || 0
+      );
+    });
 
-  console.log(AMD, USD, RUB, EUR);
+    switch (cur5) {
+      case "AMD":
+        AMD = AMD + parseFloat(cur5val.replace(/,/g, ""));
+        break;
+      case "USD":
+        USD = USD + parseFloat(cur5val.replace(/,/g, ""));
+        break;
+      case "RUB":
+        RUB = RUB + parseFloat(cur5val.replace(/,/g, ""));
+        break;
+      case "EUR":
+        EUR = EUR + parseFloat(cur5val.replace(/,/g, ""));
+        break;
 
+      default:
+      // code block
+    }
+  }
+
+  hashivner.push({Անվանում, AMD, USD, RUB, EUR})
+
+
+  
+  
+  await page.click("#VWG_145");
+}
+
+var xls = json2xls(hashivner);
+fs.writeFileSync('data.xlsx', xls, 'binary');
+// fs.writeFileSync('./data.json', JSON.stringify(hashivner), 'UTF-8');
   //   page.mainFrame().evaluate()
   // await page.mainFrame()
   // .waitForSelector("#TRG_133");
 
-  await page.screenshot({ path: "example.png" });
-  //await browser.close();
+ // await page.screenshot({ path: "example.png" });
+  await browser.close();
 })();
